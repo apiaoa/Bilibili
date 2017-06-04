9a885cf1a9734895201b053b314291603c5fc6c3
memory <547527489@qq.com>
2017-06-04 13:45:37 +0800

    Initial commit

---
+++ app/src/main/java/com/github/apiaoa/bilibili/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.apiaoa.bilibili;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
